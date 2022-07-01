fb39394a06d381425956c893aa992cc9c3878f7f
Ahmed Nefzaoui <nefzaoui.a@gmail.com>
2022-07-01 04:49:55 +0100

    added mobile table

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var vote = require('./vote.json');
 app.use(express.static('public'))
 
 
-// Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('hbs', hbs.express4({
     partialsDir: __dirname + '/views/partials',
     layoutsDir: __dirname + '/views/layouts',
@@ -28,35 +27,34 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
     let q = req.query.q;
     if (q == null || q == undefined || q.length < 4) {
-    res.render('search_error', {
-        title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نائج',
-        error: "رجاءا أدخل أكثر من 4 حروف أو أرقام في خانة البحث",
-        layout: 'main'
-    });
-} else {
-    let result = vote.filter(element => (element[0].includes(q) || element[1].includes(q) || element[2].includes(q) || element[3].includes(q) || element[4].includes(q) || element[5].includes(q) || element[6].includes(q)))
-    //console.log(result);
-
-    if (result.length > 0) {
-        res.render('search', {
-            title: 'مكتب الإقتراع بعنوان ' + q,
-            keyword: req.query.q,
-            result: result,
+        res.render('search_error', {
+            title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نائج',
+            error: "رجاءا أدخل أكثر من 4 حروف أو أرقام في خانة البحث",
             layout: 'main'
         });
     } else {
-        res.render('search_error', {
-            title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نتائج بعنوان ' + q,
-            keyword: req.query.q,
-            error: "لا يوجد نتائج",
-            layout: 'main'
-        });
-    }
-}
+        let result = vote.filter(element => (element[0].includes(q) || element[1].includes(q) || element[2].includes(q) || element[3].includes(q) || element[4].includes(q) || element[5].includes(q) || element[6].includes(q)))
+        //console.log(result);
 
+        if (result.length > 0) {
+            res.render('search', {
+                title: 'مكتب الإقتراع بعنوان ' + q,
+                keyword: req.query.q,
+                result: result,
+                layout: 'main'
+            });
+        } else {
+            res.render('search_error', {
+                title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نتائج بعنوان ' + q,
+                keyword: req.query.q,
+                error: "لا يوجد نتائج",
+                layout: 'main'
+            });
+        }
+    }
 
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Bureau Vote app is now listening on port ${port}`)
 })
