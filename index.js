cf3fabda63459c4ffab915e263ba7ff913692e78
Ahmed Nefzaoui <nefzaoui.a@gmail.com>
2022-07-03 07:09:12 +0100

    visual fixes

--- index.js
+++ index.js
@@ -1,80 +1,86 @@
-const express = require('express')
-const hbs = require('express-hbs');
-const app = express()
-const port = 3000
-const vote = require('./vote.json');
+const express = require("express");
+const hbs = require("express-hbs");
+const app = express();
+const port = 3000;
+const vote = require("./vote.json");
 
 String.prototype.contains = function (string) {
-    var keywords = string.split(" ");
-    var contain = true;
+  var keywords = string.split(" ");
+  var contain = true;
 
-    for (var i = 0; i < keywords.length && contain; i++) {
-        if (keywords[i] == "") continue;
+  for (var i = 0; i < keywords.length && contain; i++) {
+    if (keywords[i] == "") continue;
+    var regex = new RegExp(keywords[i], "i");
+    contain = contain && regex.test(this);
+  }
+  return contain;
+};
 
-        var regex = new RegExp(keywords[i], "i");
-        contain = contain && regex.test(this);
-    }
+app.use(express.static("public"));
+app.engine(
+  "hbs",
+  hbs.express4({
+    partialsDir: __dirname + "/views/partials",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
-    return contain;
-}
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "قاعدة بيانات مراكز الإقتراع في تونس والخارج",
+    layout: "main",
+  });
+});
 
-app.use(express.static('public'))
-
-app.engine('hbs', hbs.express4({
-    partialsDir: __dirname + '/views/partials',
-    layoutsDir: __dirname + '/views/layouts',
-}));
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-
-app.get('/', (req, res) => {
-    res.render('home', {
-        title: 'قاعدة بيانات مراكز الإقتراع - إبحث عن مركز إقتراع',
-        layout: 'main'
-    });
-
-})
-
-app.get('/search', (req, res) => {
-    let q = req.query.q || '';
-    let qT = q.toUpperCase()
-    .replace('المدرسة', '')
-    .replace('مدرسة', '')
-    .replace('الإبتدائية', '')
-    .replace('إبتدائية', '')
-    .replace('الابتدائية', '')
-    .replace('ابتدائية', '')
-    .replace('المكتب', '')
-    .replace('مكتب', '')
+app.get("/search", (req, res) => {
+  let q = req.query.q || "";
+  let qT = q
+    .toUpperCase()
+    .replace("المدرسة", "")
+    .replace("مدرسة", "")
+    .replace("الإبتدائية", "")
+    .replace("إبتدائية", "")
+    .replace("الابتدائية", "")
+    .replace("ابتدائية", "")
+    .replace("المكتب", "")
+    .replace("مكتب", "")
     .trim();
-    if (q == null || q == undefined || qT.length < 4) {
-        res.render('search_error', {
-            title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نائج',
-            error: "رجاءً أدخل أكثر من 4 حروف أو أرقام في خانة البحث",
-            layout: 'main'
-        });
-    } else {
-        let result = vote.filter(element => element.join().contains(qT))
+  if (q == null || q == undefined || qT.length < 4) {
+    res.render("search_error", {
+      title: "لا يوجد نائج - قاعدة بيانات مراكز الإقتراع",
+      error: "رجاءً أدخل أكثر من 4 حروف أو أرقام في خانة البحث",
+      layout: "main",
+    });
+  } else {
+    let result = vote.filter((element) => element.join().contains(qT));
 
-        if (result.length > 0) {
-            res.render('search', {
-                title: 'مكتب الإقتراع بعنوان ' + q,
-                keyword: req.query.q,
-                result: result,
-                layout: 'main'
-            });
-        } else {
-            res.render('search_error', {
-                title: 'قاعدة بيانات مراكز الإقتراع - لا يوجد نتائج بعنوان ' + q,
-                keyword: req.query.q,
-                error: "لا يوجد نتائج",
-                layout: 'main'
-            });
-        }
+    if (result.length > 0) {
+      res.render("search", {
+        title: "مكتب الإقتراع بعنوان " + q,
+        keyword: req.query.q,
+        result: result,
+        layout: "main",
+      });
+    } else {
+      res.render("search_error", {
+        title: "لا يوجد نتائج بعنوان " + q,
+        keyword: req.query.q,
+        error: "لا يوجد نتائج",
+        layout: "main",
+      });
     }
-})
+  }
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "حول الموقع - قاعدة بيانات مراكز الإقتراع",
+    layout: "main",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Bureau Vote app is now listening on port ${port}`)
-})
+  console.log(`Bureau Vote app is now listening on port ${port}`);
+});
